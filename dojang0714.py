01618a18cc93d093d7c3a55e2072930bfa8f6cb8
KIMHONGJUN2 <96hjun@naver.com>
2021-07-14 22:53:25 +0900

    코딩도장 0714_unit10 시퀀스 객체

--- dojang0714.py
+++ dojang0714.py
@@ -43,6 +43,51 @@ a,b,c, = x
 print(a,b,c)  #언패킹  = 리스트와 튜플의 요소를 변수 여러개에 할당하는 것
 
 # ------------ 10 질문 1
-a =input()
-b = tuple(range(-10,10,int(a)))
-print(b)
+# a =input()
+# b = tuple(range(-10,10,int(a)))
+# print(b)
+
+# 11 시퀀스
+a = [0,10,20,30,40,50,60,70,80,90]
+print(30 in a)
+
+b = [9,8,7,6]
+print(a+b)    # + 로 시퀀스 객체 연결 가능
+
+print(b*3)  # *가능
+
+print(len(a))  # 리스트 요소의 개수 구하기
+
+print(len(range(0,10,2))) # 0 2 4 6 8
+
+hello = '안녕하세요'
+print(len(hello)) #문자열의 공백은 포함 따옴표는 불포함
+
+print(b[0]) #인덱스 0= 첫번째 요소 출력
+print(b.__getitem__(0)) # []= __getitem__
+
+print(b[-1])  # 음수 인덱스
+print(b[len(b)-1])  #길이에서 -1을 해주어야 마지막 인덱스가 된다
+
+del b[0]
+print(b)  # 리스트의 첫번째 요소 삭제
+
+a = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(a[0:4]) # 인덱스 0부터 3까지 자름
+print(a[:6]) #처음부터 5까지
+print(a[2:]) #2부터 끝까지
+print(a[:])  # 리스트 전체 = a[::]
+print(a[7::2])
+print(a[:7:2])
+
+s = slice(4,7)
+print(a[s])
+
+# ------ 11 문제 1
+del x[-5:]
+print(tuple(x))
+
+# ------ 11 문제 2
+a = input()
+b= input()
+print(a[1::2]+b[0::2])
