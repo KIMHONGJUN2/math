969d0ba8c19dfd2bd854cfac49eaf7e633bfefe0
KIMHONGJUN2 <96hjun@naver.com>
2021-07-14 21:12:37 +0900

    코딩도장 0714_unit8

---
+++ dojang0714.py
@@ -0,0 +1,13 @@
+# 8. 불과 비교 연산자
+print('python' == 'python') #문자열 비교
+print(1 is 1.0) # is is not 은 객체를 비교
+print(True and True)
+print(True or True)
+print(not True)   # not and or 순으로 판단
+print(10 == 10 and 10 !=5 )
+print(False or 'python')
+
+a,b,c,d = map(int,input().split())
+print(a>=90 and b>80 and c>85 and d>=80)
+
+# 9. 문자열 사용하기
