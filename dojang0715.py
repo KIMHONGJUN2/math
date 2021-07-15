e7ee8e851310877c219f64f9b3f86371bc4801b3
KIMHONGJUN2 <96hjun@naver.com>
2021-07-15 21:02:24 +0900

    코딩도장 0714_unit 14 else문

--- dojang0715.py
+++ dojang0715.py
@@ -7,15 +7,26 @@ lux = {'health':500,'mana':334,'melee':550} #딕셔너리 키:값 형태
 #c = dict(zip(a,b))
 #print(c)
 
-# if 조건문
+# 13.if 조건문
 x = 10
 if x==10 :
     pass # TODO x가 10일 때 처리 필요
 
-a=int(input())
-b2=input()
-if b2 == 'Cash3000':
-    a-=3000
-if b2 =='Cash5000':
-    a-=5000
-print(a)
+# a=int(input())
+# b2=input()
+# if b2 == 'Cash3000':
+#     a-=3000
+# if b2 =='Cash5000':
+#     a-=5000
+# print(a)
+
+#14. else
+q,w,e,r = map(int,input().split())
+if 0>q or q>100 or 0>r or r>100 or 0>e or e>100 or 0>w or w>100:
+    print('잘못된 점수')
+else:
+    if (q+w+e+r)/4 >=80:
+        print('합격')
+
+    else :
+        print('불합격')
