11975658772449b243d9ed47fcef043e61b493e4
KIMHONGJUN2 <96hjun@naver.com>
2021-07-15 20:39:09 +0900

    코딩도장 0714_unit 13 if문

--- dojang0715.py
+++ dojang0715.py
@@ -2,9 +2,20 @@
 lux = [490,334,550,19.72]  # 각 값이 어떤 것을 의미하는지 알기 어려움
 lux = {'health':500,'mana':334,'melee':550} #딕셔너리 키:값 형태
 # ------- 12 문제 1
-a = list(input().split())
-b = list(map(float,input().split()))
-c = dict(zip(a,b))
-print(c)
+#a = list(input().split())
+#b = list(map(float,input().split()))
+#c = dict(zip(a,b))
+#print(c)
 
 # if 조건문
+x = 10
+if x==10 :
+    pass # TODO x가 10일 때 처리 필요
+
+a=int(input())
+b2=input()
+if b2 == 'Cash3000':
+    a-=3000
+if b2 =='Cash5000':
+    a-=5000
+print(a)
