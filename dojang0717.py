34aeb5c2fcd7020ad1be8619bad39a809f21a16b
KIMHONGJUN2 <96hjun@naver.com>
2021-07-17 22:08:46 +0900

    코딩도장 0714_unit 23 2차원 리스트

--- dojang0717.py
+++ dojang0717.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 # 20 fizzbuzz
 # for i in range(1,101):
 #     if i % 3 == 0 and i % 5 ==0:
@@ -89,9 +90,73 @@
 #     print(index, value)  #인덱스와 요소 같이 출력 enumerate 사용
 
 # TODO 22 심사문제
-a,b = map(int,input().split())
-re=[]
-for i in range(a,b+1):
-    re.append(2**i)
-del re[1], re[-2]
-print(re)
+# a,b = map(int,input().split())
+# re=[]
+# for i in range(a,b+1):
+#     re.append(2**i)
+# del re[1], re[-2]
+# print(re)
+
+# 23. 2차원 리스트
+# a = [[10, 20],
+#      [30, 40],
+#      [50, 60] ]
+#
+# a = [[10, 20],
+#      [500, 600, 700],
+#      [9],
+#      [30, 40],
+#      [8],
+#      [800, 900, 1000]]  #톱니형 리스트(jagged list)
+#
+
+# pprint(a, indent=4, width=20)
+#
+# a = []  # 빈 리스트 생성
+#
+# for i in range(3):
+#     line = []  # 안쪽 리스트로 사용할 빈 리스트 생성
+#     for j in range(2):
+#         line.append(0)  # 안쪽 리스트에 0 추가
+#     a.append(line)  # 전체 리스트에 안쪽 리스트를 추가
+#
+# print(a)  # 반복문으로 2차원 리스트 만들기
+#
+# students = [
+#     ['john', 'C', 19],
+#     ['maria', 'A', 25],
+#     ['andrew', 'B', 7]
+# ]
+#
+# print(sorted(students, key=lambda student: student[1]))  # 안쪽 리스트의 인덱스 1을 기준으로 정렬
+# print(sorted(students, key=lambda student: student[2]))  # 안쪽 리스트의 인덱스 2를 기준으로 정렬
+#
+# a = [[10, 20], [30, 40]]
+# import copy             # copy 모듈을 가져옴
+# b = copy.deepcopy(a) # 다차원 리스트는 deepcopy 이용
+
+# TODO 23 심사 문제 -------     ☆☆☆☆☆다시 보기☆☆☆☆☆ = 지뢰찾기
+row,col = map(int,input().split())
+matrix = []
+for i in range(row):
+    matrix.append(list(input()))
+for i in range(row):
+    for j in range(col):
+
+        if matrix[i][j] == '*':
+            continue
+        else:
+            matrix[i][j]=int(0)
+for i in range(row):
+    for j in range(col):
+        for y in range(i-1,i+2):
+            for x in range(j-1,j+2):
+                if y<0 or x<0 or y >= row or x >= col:
+                    pass
+                elif matrix[i][j] != '*' and matrix[y][x] == '*':
+                    matrix[i][j] +=1
+for i in range(row):
+    for j in range(col):
+        print(matrix[i][j],end='')
+    print()
+
