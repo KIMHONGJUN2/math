fcfbda033335be28df2462aa44cd7c8f15f19f81
KIMHONGJUN2 <96hjun@naver.com>
2021-07-17 17:49:56 +0900

    코딩도장 0714_unit 21 사각형 그리기

--- dojang0717.py
+++ dojang0717.py
@@ -16,15 +16,58 @@
 
 # TODO 20 심사문제
 
-a,b = map(int,input().split())
-for i in range(a,b+1):
-    if i % 5 ==0 and i % 7 == 0:
-        print('FizzBuzz')
-    elif i % 5 == 0:
-        print('Fizz')
-    elif i % 7 == 0:
-        print('Buzz')
-    else:
-        print(i)
+# a,b = map(int,input().split())
+# for i in range(a,b+1):
+#     if i % 5 ==0 and i % 7 == 0:
+#         print('FizzBuzz')
+#     elif i % 5 == 0:
+#         print('Fizz')
+#     elif i % 7 == 0:
+#         print('Buzz')
+#     else:
+#         print(i)
 
+# 21. 사각형 그리기
+
+
+import turtle as t
+t.shape('turtle')  # 사각형 그리기
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+#
+# for i in range(5):      # 오각형이므로 5번 반복
+#     t.forward(100)
+#     t.right(360 / 5)
+
+
+# n = 60    # 원을 60번 그림
+# t.shape('turtle')
+# t.speed('fastest')      # 거북이 속도를 가장 빠르게 설정
+# for i in range(n):
+#     t.circle(120)       # 반지름이 120인 원을 그림
+#     t.right(360 / n)    # 오른쪽으로 6도 회전
+#
+# t.shape('turtle')
+# t.speed('fastest')
+# for i in range(300):
+#     t.forward(i)
+#     t.right(80)
+
+# TODO 21 심사문제
+
+n, line = map(int, input().split())
+t.shape('turtle')
+t.speed('fastest')
+for i in range(n):
+    t.forward(line)
+    t.right(360/n*2)
+    t.forward(line)
+    t.left(360/n)
+
+t.mainloop()
 
