918054bdbbf2af0beb1f9abd2045061114275a98
KIMHONGJUN2 <96hjun@naver.com>
2021-07-17 20:42:26 +0900

    코딩도장 0714_unit 22 리스트

--- dojang0717.py
+++ dojang0717.py
@@ -30,8 +30,8 @@
 # 21. 사각형 그리기
 
 
-import turtle as t
-t.shape('turtle')  # 사각형 그리기
+# import turtle as t
+# t.shape('turtle')  # 사각형 그리기
 # t.fd(100)
 # t.rt(90)
 # t.fd(100)
@@ -60,14 +60,38 @@ t.shape('turtle')  # 사각형 그리기
 
 # TODO 21 심사문제
 
-n, line = map(int, input().split())
-t.shape('turtle')
-t.speed('fastest')
-for i in range(n):
-    t.forward(line)
-    t.right(360/n*2)
-    t.forward(line)
-    t.left(360/n)
+# n, line = map(int, input().split())
+# t.shape('turtle')
+# t.speed('fastest')
+# for i in range(n):
+#     t.forward(line)
+#     t.right(360/n*2)
+#     t.forward(line)
+#     t.left(360/n)
+#
+# t.mainloop()
 
-t.mainloop()
 
+# 22. 리스트 조작하기
+
+# a = [10, 20, 30]
+# a.append([500, 600])
+# print(len(a))
+#
+# a = [0, 0, 0, 0, 0]
+# b = a  #변수만 다를 뿐 리스트는 한개이다
+#
+# a = [0, 0, 0, 0, 0]
+# b = a.copy()  #리스트 복사 = > 새로운 리스트 생성
+#
+# a = [38, 21, 53, 62, 19]
+# for index, value in enumerate(a):
+#     print(index, value)  #인덱스와 요소 같이 출력 enumerate 사용
+
+# TODO 22 심사문제
+a,b = map(int,input().split())
+re=[]
+for i in range(a,b+1):
+    re.append(2**i)
+del re[1], re[-2]
+print(re)
