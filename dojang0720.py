caf586555d3c26f2a5ad0a0c5db45cde77cd5b49
KIMHONGJUN2 <96hjun@naver.com>
2021-07-20 23:09:26 +0900

    코딩도장 0714_unit 27 파일 가져오기

---
+++ dojang0720.py
@@ -0,0 +1,21 @@
+# 27. 파일 사용하기
+# file = open('hello.txt', 'w')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
+# file.write('Hello, world!')      # 파일에 문자열 저장
+# file.close()                     # 파일 닫기
+#
+# file = open('hello.txt', 'r')    # hello.txt 파일을 읽기 모드(r)로 열기. 파일 객체 반환
+# s = file.read()                  # 파일에서 문자열 읽기
+# print(s)                         # Hello, world!
+# file.close()                     # 파일 객체 닫기
+#
+# with open('hello.txt', 'r') as file:    # hello.txt 파일을 읽기 모드(r)로 열기
+#     s = file.read()                     # 파일에서 문자열 읽기
+#     print(s)                            # Hello, world!  // 자동으로 파일 객체 닫기
+
+# TODO 27 심사문제
+with open("C:/Users/82105/PycharmProjects/mathstudy/words.txt",'r') as file:
+    words = file.read()
+    wr = words.split(' ')
+    for word in wr:
+        if 'c' in word:
+            print(word.strip(',.'))
