3c804f1b35dde162ce17642e20919e2a901b91e2
KIMHONGJUN2 <96hjun@naver.com>
2021-07-27 23:57:22 +0900

    코딩도장 0714_unit 32 람다

--- dojang 0726.py
+++ dojang 0726.py
@@ -2,5 +2,25 @@
 # def plus_ten(x):
 #     return x + 10
 
-plus_ten = lambda x: x + 10
-print(plus_ten(1))
+# plus_ten = lambda x: x + 10
+# print(plus_ten(1))
+
+# # TODO 32 람다 표현
+# files = input().split()
+# print(map(list(lambda x : format{0:03d}.{1})))
+
+# 33
+# def countdown(n):
+#     def count():
+#         nonlocal n
+#         n = n-1
+#         return n
+#     return count()
+#
+# n = int(input())
+#
+# c = countdown(n)
+# for i in range(n):
+#     print(c(), end=' ')
+
+# 34 클래스 객체 만들기
